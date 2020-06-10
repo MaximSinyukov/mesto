e9889136d16ceace946e62f82c6f209d4f3bee43
Maxim Sinyukov <verero1173@yandex.ru>
2020-06-10 21:04:57 +0300

    fix: изменить функцию для esc и поменять класс инпутов

--- validate.js
+++
@@ -1,6 +0,0 @@
-function enableValidation(options) {
- const formList = Array.from(document.querySelectorAll(`${options.formSelector}`));
- formList.forEach((form) => {
-  setEventListeners(form);
- });
-};
