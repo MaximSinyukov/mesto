2d5a52a3ee3a5bb81f593b8cb41e86699fb426d5
Maxim Sinyukov <verero1173@yandex.ru>
2020-06-10 21:05:44 +0300

    fix: изменить функцию для esc и поменять класс инпутов

---
+++ scripts/validate.js
@@ -0,0 +1,54 @@
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(`${formValidationOptions.inputSelector}`));
+  const buttonElement = formElement.querySelector(`${formValidationOptions.submitButtonSelector}`);
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(`${formValidationOptions.inactiveButtonClass}`);
+    buttonElement.setAttribute('disabled', true);
+  } else {
+    buttonElement.classList.remove(`${formValidationOptions.inactiveButtonClass}`);
+    buttonElement.removeAttribute('disabled');
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(`${formValidationOptions.inputErrorClass}`);
+  errorElement.textContent = errorMessage;
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(`${formValidationOptions.inputErrorClass}`);
+  errorElement.textContent = '';
+};
+
+function enableValidation(options) {
+  const formList = Array.from(document.querySelectorAll(`${options.formSelector}`));
+  formList.forEach((form) => {
+    setEventListeners(form);
+  });
+}
